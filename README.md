83dd9d29cda9f5000c8a6b4f641165115a01accc
Otatopot <rochelewebs24@gmail.com>
2024-01-05 18:33:28 -0500

    Edits made and README updated

---
+++ README.md
@@ -0,0 +1,6 @@
+#Rock Paper Scissors python program
+
+This program was essentially created to simulate rock paper scissors through code.
+
+##Randomised RPS
+Includes code for a randomized version of rock paper scissors which takes no input from the user. By simply running the code, the signs for 2 players are chosen and the results are determined and displayed.
--- randomised RPS.py
+++ randomised RPS.py
@@ -7,19 +7,18 @@ def run():
     options = list((rock, paper, scissors))    
     x = random.randrange(0, 3) #remember range doesn't include last figure
     y = random.randrange(0, 3) 
-    print(x,y)
     signx = options[x]
     signy = options[y]
     print("Player 1 chose " + signx + " and Player 2 chose " + signy)
     #while y != 2:
     if x == y+1:
-        print(signx + " beats "  + signy + " Player 1 Wins!!!")
+        print(signx + " beats "  + signy + "! Player 1 Wins!!!")
     elif y == 2 and x == 0:
-        print(signx + " beats "  + signy + " Player 1 Wins!!!")
+        print(signx + " beats "  + signy + "! Player 1 Wins!!!")
     elif y == x+1:
-        print(signy + " beats "  + signx + " Player 2 Wins!!!")
+        print(signy + " beats "  + signx + "! Player 2 Wins!!!")
     elif x == 2 and y == 0:
-        print(signy + " beats "  + signx + " Player 2 Wins!!!")
+        print(signy + " beats "  + signx + "! Player 2 Wins!!!")
     else:
         print("It's a TIE!!!!")
 
