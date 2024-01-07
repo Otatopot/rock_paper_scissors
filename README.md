c81a203bc0a7fc0520141e8447f036f21087136e
Otatopot <rochelewebs24@gmail.com>
2024-01-06 22:10:10 -0500

    Slightly modified logic

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 
 This program was essentially created to simulate rock paper scissors through code.
 
-##Randomised RPS
+##Randomised RPS - Started 5/1/2024
 Includes code for a randomized version of rock paper scissors which takes no input from the user. By simply running the code, the signs for 2 players are chosen and the results are determined and displayed.
+
+6/1/2024
+Edit - Simplified the code for the randomized algorithm.
--- randomised RPS.py
+++ randomised RPS.py
@@ -11,16 +11,17 @@ def run():
     signy = options[y]
     print("Player 1 chose " + signx + " and Player 2 chose " + signy)
     #while y != 2:
-    if x == y+1:
-        print(signx + " beats "  + signy + "! Player 1 Wins!!!")
-    elif y == 2 and x == 0:
-        print(signx + " beats "  + signy + "! Player 1 Wins!!!")
-    elif y == x+1:
-        print(signy + " beats "  + signx + "! Player 2 Wins!!!")
-    elif x == 2 and y == 0:
+    if x == y+1 or (y == 2 and x == 0):
+        print(signx + " beats "  + signy + "! Player 1 Wins!!!")    
+    elif y == x+1 or (x == 2 and y == 0):
         print(signy + " beats "  + signx + "! Player 2 Wins!!!")
     else:
         print("It's a TIE!!!!")
+    #elif y == 2 and x == 0:
+    #    print(signx + " beats "  + signy + "! Player 1 Wins!!!")
+    #elif x == 2 and y == 0:
+    #    print(signy + " beats "  + signx + "! Player 2 Wins!!!")
+    
 
 run()
     
